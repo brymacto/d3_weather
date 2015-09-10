3311922ab6978fda8ab441ffd074a0f6c2a47671
Bryan MacDiarmid <brymac@gmail.com>
2015-09-10 13:45:48 -0400

    JSON data displaying on page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,6 @@ class StaticPagesController < ApplicationController
 
   def weather
     response = HTTParty.get("http://api.openweathermap.org/data/2.5/history/city?q=Toronto,CA&APPID=#{ENV['open_weather_key']}")
-    puts response.body, response.code, response.message, response.headers.inspect
+    @hours = response['list']
   end
 end
