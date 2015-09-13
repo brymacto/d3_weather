1cca4f35dbe7878e8d3aea677b07a24e37aba86a
Bryan MacDiarmid <brymac@gmail.com>
2015-09-13 17:26:07 -0400

    get_hours added to cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,23 +1,39 @@
 class CitiesController < ApplicationController
+  include HTTParty
   def index
     @cities = City.all.order(name: :asc)
   end
 
   def show
+    @city = City.find(params[:id])
   end
 
   def edit
+    @city = City.find(params[:id])
   end
 
   def create
     @city = City.new(city_params)
 
     if @city.save
+      @city.get_hours
       redirect_to cities_path
     else
       render 'new'
     end
   end
+
+  def get_hours
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/history/city?q=Toronto,CA&APPID=#{ENV['open_weather_key']}")
+    hours_details = response['list']
+    @hours = []
+    hours_details.each do |hour_details|
+      @hours << Hour.create()
+    end
+    puts @hours
+  end
+  # helper_method :get_hours
+  # Uncomment the above to access method from view.
 
   def new
     @city = City.new
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,8 +1,8 @@
 class City < ActiveRecord::Base
-  include HTTParty
-  base_uri 'api.openweathermap.org/'
+  # include HTTParty
+  # base_uri 'api.openweathermap.org/'
   has_many :hours, dependent: :destroy
 
-  attr_accessor :name, :country, :open_weather_id
-  attr_reader :id
+
+
 end
